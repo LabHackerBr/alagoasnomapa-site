284fd0fe5af486aff0f68c89ba595d414142a3a9
Eduardo Liron <eduardoliron@gmail.com>
2022-04-06 18:11:54 -0300

    feat(index): add temporary under construction page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,1 +1,8 @@
-<div class="flex w-full h-full items-center justify-center">Em construção</div>
+<div class="w-full h-full max-w-4xl px-8 flex flex-col items-center justify-center mx-auto">
+  <img src="/logo_full.jpg" alt="logo do projeto representando o mapa de alagoas" />
+  <h1 class="mb-12">Em construção</h1>
+  <p><b>Alagoas no Mapa</b> &eacute; um projeto que estimula a cultura de
+    colabora&ccedil;&atilde;o e participa&ccedil;&atilde;o em meios digitais nos
+    munic&iacute;pios alagoanos.<br /><br />
+    Na primeira etapa, Entre Lagoas, o primeiro município visitado foi <a href="/coqueiroseco">Coqueiro Seco</a>.</p>
+</div>
