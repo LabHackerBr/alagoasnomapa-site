e6ef6a98668f58ea5a8ce3f6c4077521db9e266b
Eduardo Liron <eduardoliron@gmail.com>
2022-12-02 10:09:36 -0300

    deploy

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,23 @@
 import adapter from '@sveltejs/adapter-static'
 import preprocess from 'svelte-preprocess'
 
+const dev = "production" === "development";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	kit: {
-		prerender: {default: true},
 		adapter: adapter({
 			pages: "docs",
 			assets: "docs",
-			fallback: false
+      fallback: 'index.html',
+      precompress: false
 		}),
+    paths: {
+      // change below to your repo name
+      base: dev ? "" : "/alagoasnomapa-site",
+    },
 	},
 
   preprocess: [
