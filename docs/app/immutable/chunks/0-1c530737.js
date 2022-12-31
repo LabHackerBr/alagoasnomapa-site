311f5a81195c83da143234cf696c78c169d4a047
Eduardo Liron <eduardoliron@gmail.com>
2022-12-30 22:06:16 -0300

    publish main page

--- docs/app/immutable/chunks/0-1c530737.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_layout.svelte-c6a467bf.js";export{t as component};
---
+++ docs/app/immutable/chunks/0-fcc7bca0.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_layout.svelte-6c523183.js";export{t as component};
